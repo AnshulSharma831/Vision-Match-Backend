62b4378e1bfdb990e9242349d19d36919bf0eb8c
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2025-09-01 00:42:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://vision-match-frontend.vercel.app'],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
     credentials: true
 }));
 
